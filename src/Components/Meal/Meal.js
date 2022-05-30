c0c405a124337b1ef9e9a2edcab560c634b82211
Mehedi Hasan Ashik <mhashik.ashik@gmail.com>
2022-05-30 11:49:42 +0600

    pop-up added

--- src/Components/Meal/Meal.js
+++ src/Components/Meal/Meal.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import './Meal.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight, faCoffee } from '@fortawesome/free-solid-svg-icons'
-
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import Popup from 'reactjs-popup';
 
 const Meal = (props) => {
     console.log(props.meal)
-    const {strMeal, strArea, strCategory, strMealThumb, } = props.meal
+    const { strMeal, strArea, strCategory, strMealThumb, strInstructions } = props.meal;
+
+
+    const PopupExample = () => (
+        <Popup trigger={<button><FontAwesomeIcon icon={faArrowRight} /> Recipe </button>} position='top center'>
+            {close => (
+                <div>
+                    {strInstructions}
+                    <a className="close" onClick={close}>
+                        &times;
+                    </a>
+                </div>
+            )}
+        </Popup>
+    );
+
+
     return (
         <div className='meal-container'>
             <img src={strMealThumb} alt="" />
             <h2>{strMeal}</h2>
             <h4>Food Origin: {strArea}</h4>
             <h4>Food Category: {strCategory}</h4>
-            <button><FontAwesomeIcon icon={faArrowRight}/> Instructions</button>
+            <PopupExample />
+
         </div>
+
     );
+
 };
 
 export default Meal;
